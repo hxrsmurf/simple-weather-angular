84cd6616b7691c26f7606a26cb7ba6173d0de114
Kevin Murphy <kevin@hxrsmurf.info>
2021-07-05 15:48:22 -0500

    Add weather.gov functionality

--- src/app/components/weather/weather.component.ts
+++ src/app/components/weather/weather.component.ts
@@ -1,3 +1,4 @@
+import { HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { IpserviceService } from 'src/app/services/ipservice.service';
 import { WeatherService } from 'src/app/services/weather.service';
@@ -10,9 +11,18 @@ import { WeatherService } from 'src/app/services/weather.service';
 
 export class WeatherComponent implements OnInit {
   ipAddress: string = '';
-  city: string = '';
+  zipcode: string = '';
+  city: string = '';   
+  latitude: string = '';
+  longitude: string = '';
+
+  // Open Weather
   temp: string = '';
   realFeal: string = '';
+
+  // Gov. Weather
+  currentGovWeather: string = '';
+  weatherGovURL: string = ''; 
 
   constructor(
     private ip: IpserviceService,
@@ -27,10 +37,25 @@ export class WeatherComponent implements OnInit {
     this.ip.getIPAddress().subscribe((res: any) => {
       this.ipAddress = res.ip;
       this.city = res.city;
-      this.weather.manualWeather(res.zip_code).subscribe((res: any) => {
+      this.latitude = res.latitude;
+      this.longitude = res.longitude;
+      this.zipcode = res.zip_code
+
+      // openweathermap.org
+      this.weather.manualWeather(this.zipcode).subscribe((res: any) => {
         this.temp = res.main.temp
         this.realFeal = res.main.feels_like
       })
-    })
+
+      // weather.gov
+      this.weatherGovURL = `https://forecast.weather.gov/MapClick.php?lon=${this.longitude}&lat=${this.latitude}`
+      this.weather.weatherGov(this.latitude, this.longitude).subscribe((res:any)=>{
+        let forecastURL = res.properties.forecast
+        this.weather.weatherForecast(forecastURL).subscribe((res:any)=>{
+          let weatherPropertyPeriods = res.properties.periods
+          this.currentGovWeather = weatherPropertyPeriods[0].temperature
+        })
+      })
+    });
   }
 }
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { IpserviceService } from './ipservice.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class WeatherService{
+export class WeatherService {
   stringify: string = ''
   env = environment;
   constructor(
@@ -17,24 +17,31 @@ export class WeatherService{
   ngOnInit(): void {
   }
 
-  manualWeather(zipcode: string){
+  manualWeather(zipcode: string) {
     let api = this.env.weather.key;
     let url = `https://api.openweathermap.org/data/2.5/weather?zip=${zipcode}&appid=${api}&units=imperial`
-    console.log(url)
     return this.http.get(url)
   }
 
-  getWeatherZip(){
-    this.ip.getIPAddress().subscribe((res:any)=>{
+  //weatherGov(lat: string, long: string){
+  weatherGov(lat: string, long: string) {
+    let url = `https://api.weather.gov/points/${lat},${long}`;
+    return this.http.get(url)
+  }
+
+  weatherForecast(url: string){
+    return this.http.get(url)
+  }
+
+  getWeatherZip() {
+    this.ip.getIPAddress().subscribe((res: any) => {
       let zip = res.zip_code;
       let api = this.env.weather.key;
       let url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${api}&units=imperial`;
       let result = this.http.get(url);
-      console.log(url)
-      result.subscribe((res:any)=>{
+      result.subscribe((res: any) => {
         this.stringify = JSON.stringify(res)
-        //console.log(this.stringify)
       })
-    })    
+    })
   }
 }
